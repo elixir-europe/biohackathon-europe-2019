7af8ee5a11404395be4df389be4e9754039ade95
Ric <ricartomojo@gmail.com>
2019-07-19 14:28:25 +0100

    Correct merged from for project 1 and 9 on source file

--- scripts/repo_gen.py
+++ scripts/repo_gen.py
@@ -12,6 +12,7 @@ PROJECTS_REPOSITORY = "https://github.com/elixir-europe/BioHackathon-projects-20
 ACPT_PROJECT_NUMBER = "number"
 ACPT_PROJECT_MERGED = "merged_from"
 ACPT_PROJECT_TITLE = "title"
+ACPT_PROJECT_SUBMITTER = "submitter"
 
 # EASY CHAIR DUMP PROJECT COLUMNS
 PROJECT_NUMBER = "#"
@@ -93,7 +94,10 @@ def to_file(project):
 
     with open(file_name, "w+") as output_file:
         output_file.write("# {}\n\n".format(project.get("title")))
-
+        
+        output_file.write(
+            "**Project Number:** {}\n\n".format(project.get("number")))
+                
         output_file.write("## Research area alignment\n\n")
         research_alignment = "\n".join(
             "- {}".format(project.get("research_area_alignment")) for project in project.get("merged_from"))
@@ -102,11 +106,16 @@ def to_file(project):
         if len(project.get("merged_from")) > 1:
             output_file.write("\n\n## Merged from\n\n")
             merged_titles = "\n".join("- {}".format(project.get("title"))
-                                    for project in project.get("merged_from"))
+                                      for project in project.get("merged_from"))
             output_file.write(merged_titles)
 
-        output_file.write("\n\n## Team")
-        output_file.write("\n\n### Proponent(s)\n\n")
+        output_file.write("\n\n## Team\n\n")
+
+        output_file.write(
+            "**Submitter:** {}\n\n".format(project.get("submitter")))
+
+        output_file.write("### Proponent(s)\n\n")
+
         authors = "\n".join("- {}".format(project.get("authors"))
                             for project in project.get("merged_from"))
         output_file.write(authors)
@@ -188,6 +197,7 @@ def main():
         pro = dict(
             number=acpt_project.get(ACPT_PROJECT_NUMBER),
             title=acpt_project.get(ACPT_PROJECT_TITLE),
+            submitter=acpt_project.get(ACPT_PROJECT_SUBMITTER),
             link=project_link,
             merged_from=merged_from_yaml
         )
